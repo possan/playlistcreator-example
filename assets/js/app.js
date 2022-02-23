e65df239ce59c38b9e618a5ae572e2d2ae563f55
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2022-02-22 21:00:36 -0300

    Restore to Create Playlist

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,148 +1,221 @@
+(function(exports) {
 
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
+	var cache = new WordCache();
 
- var express = require('express'); // Express web server framework
- var request = require('request'); // "Request" library
- var cors = require('cors');
- var querystring = require('querystring');
- var cookieParser = require('cookie-parser');
- 
- var client_id = '2a8909c2980143c496833de968725bf0'; // Your client id
- var client_secret = '90cbdda3e056407bbd8051d711f6047a'; // Your secret
- var redirect_uri = 'https://dev.portalurbanna.com.br/callback'; // Your redirect uri
- 
- /**
-  * Generates a random string containing numbers and letters
-  * @param  {number} length The length of the string
-  * @return {string} The generated string
-  */
- var generateRandomString = function(length) {
-   var text = '';
-   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
- 
-   for (var i = 0; i < length; i++) {
-     text += possible.charAt(Math.floor(Math.random() * possible.length));
-   }
-   return text;
- };
- 
- var stateKey = 'spotify_auth_state';
- 
- var app = express();
- 
- app.use(express.static(__dirname + '/public'))
-    .use(cors())
-    .use(cookieParser());
- 
- app.get('/login', function(req, res) {
- 
-   var state = generateRandomString(16);
-   res.cookie(stateKey, state);
- 
-   // your application requests authorization
-   var scope = 'user-read-private user-read-email';
-   res.redirect('https://accounts.spotify.com/authorize?' +
-     querystring.stringify({
-       response_type: 'code',
-       client_id: client_id,
-       scope: scope,
-       redirect_uri: redirect_uri,
-       state: state
-     }));
- });
- 
- app.get('/callback', function(req, res) {
- 
-   // your application requests refresh and access tokens
-   // after checking the state parameter
- 
-   var code = req.query.code || null;
-   var state = req.query.state || null;
-   var storedState = req.cookies ? req.cookies[stateKey] : null;
- 
-   if (state === null || state !== storedState) {
-     res.redirect('/#' +
-       querystring.stringify({
-         error: 'state_mismatch'
-       }));
-   } else {
-     res.clearCookie(stateKey);
-     var authOptions = {
-       url: 'https://accounts.spotify.com/api/token',
-       form: {
-         code: code,
-         redirect_uri: redirect_uri,
-         grant_type: 'authorization_code'
-       },
-       headers: {
-         'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-       },
-       json: true
-     };
- 
-     request.post(authOptions, function(error, response, body) {
-       if (!error && response.statusCode === 200) {
- 
-         var access_token = body.access_token,
-             refresh_token = body.refresh_token;
- 
-         var options = {
-           url: 'https://api.spotify.com/v1/me',
-           headers: { 'Authorization': 'Bearer ' + access_token },
-           json: true
-         };
- 
-         // use the access token to access the Spotify Web API
-         request.get(options, function(error, response, body) {
-           console.log(body);
-         });
- 
-         // we can also pass the token to the browser to make requests from there
-         res.redirect('/#' +
-           querystring.stringify({
-             access_token: access_token,
-             refresh_token: refresh_token
-           }));
-       } else {
-         res.redirect('/#' +
-           querystring.stringify({
-             error: 'invalid_token'
-           }));
-       }
-     });
-   }
- });
- 
- app.get('/refresh_token', function(req, res) {
- 
-   // requesting access token from refresh token
-   var refresh_token = req.query.refresh_token;
-   var authOptions = {
-     url: 'https://accounts.spotify.com/api/token',
-     headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-     form: {
-       grant_type: 'refresh_token',
-       refresh_token: refresh_token
-     },
-     json: true
-   };
- 
-   request.post(authOptions, function(error, response, body) {
-     if (!error && response.statusCode === 200) {
-       var access_token = body.access_token;
-       res.send({
-         'access_token': access_token
-       });
-     }
-   });
- });
- 
- console.log('Listening on 8888');
- app.listen(8888);
+	var g_name = '';
+	var g_tracks = '';
+
+	function setStatus(text) {
+		if(text != ''){
+			$('#status').html(
+				'<progress class="progress is-primary is-small" max="100">15%</progress>'
+			);
+		} else {
+			$('#status').html('');
+		}
+	}
+
+	var Playlist = function() {
+	}
+
+	var splitText = function(inputtext) {
+		var words = inputtext
+			.split(/[ \n\r\t]/)
+			.map(function(w) { return w.toLowerCase().trim().replace(/^[.,-]+/,'').replace(/[.,-]+$/g,''); })
+			.filter(function(w) { return (w.length > 0); });
+		words = words.slice(0, 100);
+		return words;
+	}
+
+	var refreshText = function() {
+		setStatus('Loading...');
+
+		g_name = $('#alltext').val().trim();
+		var words = splitText(g_name);
+		console.log('text changed.', g_name, words);
+		cache.lookupWords(words, function(worddata) {
+			setStatus('');
+
+			console.log('wordcache callback', worddata);
+			// $('#debug').text(JSON.stringify(worddata, null, 2));
+
+			var txt = '';
+			g_tracks = [];
+			worddata.forEach(function(data) {
+				console.log('word', data.word);
+
+				data.tracks.sort(function(a,b) {
+					return Math.random() - 0.5;
+				})
+
+				var names = data.tracks.map(function(track) {
+					return track.name;
+				});
+
+				console.log('names', names);
+
+				var f = new FuzzySet(names);
+				var fr = f.get(data.word);
+				console.log('fr', fr);
+
+				var found = null;
+				var title = '';
+
+				if (!found) {
+					data.tracks.forEach(function(track) {
+						if (track.name.toLowerCase().trim() === data.word.toLowerCase().trim()) {
+							found = track;
+						}
+					});
+				}
+
+				if (!found) {
+					if (fr && fr.length > 0) {
+						data.tracks.forEach(function(track) {
+							if (track.name === fr[0][1]) {
+								found = track;
+							}
+						});
+					}
+				}
+
+				if (!found) {
+					if (data.tracks.length > 0) {
+						found = data.tracks[0];
+					}
+				}
+
+				console.log('found', found);
+				if (found) {
+					g_tracks.push(found.uri);
+					txt += 
+						'<div class="media">' +
+							'<a class="pull-left" href="#">' +
+								'<img class="media-object" src="' + found.cover_url + '" />' +
+							'</a>' +
+							'<div class="media-body">' + 	
+								'<h4 class="media-heading">'+
+									'<a href="' + found.uri + '">' + found.name + '</a>'+
+								'</h4>' +
+								'Album: <a href="' + found.album_uri + '">' + found.album +'</a>'+
+								'<br />Artist: <a href="' + found.artist_uri + '">' + found.artist+'</a>' +
+							'</div>' +
+						'</div>\n';
+				} else {
+					txt += '<div class="media">No match found for the word "' + data.word+ '"</div>\n'
+				}
+			});
+
+			$('#debug').html(txt);
+		});
+	}
+
+	var doSearch = function(word, callback) {
+		console.log('search for ' + word);
+		var url = 'https://api.spotify.com/v1/search?type=track&limit=50&q=' + encodeURIComponent('track:"'+word+'"');
+		$.ajax(url, {
+			dataType: 'json',
+			success: function(r) {
+				console.log('got track', r);
+				callback({
+					word: word,
+					tracks: r.tracks.items
+						.map(function(item) {
+							var ret = {
+								name: item.name,
+								artist: 'Unknown',
+								artist_uri: '',
+								album: item.album.name,
+								album_uri: item.album.uri,
+								cover_url: '',
+								uri: item.uri
+							}
+							if (item.artists.length > 0) {
+								ret.artist = item.artists[0].name;
+								ret.artist_uri = item.artists[0].uri;
+							}
+							if (item.album.images.length > 0) {
+								ret.cover_url = item.album.images[item.album.images.length - 1].url;
+							}
+							return ret;
+						})
+				});
+			},
+			error: function(r) {
+				callback({
+					word: word,
+					tracks: []
+				});
+			}
+		});
+	}
+
+	var resolveOneWord = function() {
+		var word = cache.pop();
+		if (word) {
+			console.log('time to resolve word', word);
+			setStatus('Looking up the word "' + word + '"');
+			doSearch(word, function(result) {
+				console.log('got word result', result);
+				cache.store(word, result);
+				setTimeout(resolveOneWord, 1);
+			});
+		} else {
+			console.log('no words to look up...');
+			setTimeout(resolveOneWord, 1000);
+		}
+	}
+
+	var g_access_token = '';
+	var g_username = '';
+
+	var client_id = '';
+	var redirect_uri = '';
+
+
+	if (location.host == 'localhost:8888') {
+		client_id = '2a8909c2980143c496833de968725bf0';
+		redirect_uri = 'http://localhost:8888/callback.html';
+	} else {
+		client_id = '2a8909c2980143c496833de968725bf0';
+		redirect_uri = 'https://dev.portalurbanna.com.br/callback.html';
+	}
+
+	var doLogin = function(callback) {
+		var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
+			'&response_type=token' +
+			'&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
+			'&redirect_uri=' + encodeURIComponent(redirect_uri);
+		localStorage.setItem('createplaylist-tracks', JSON.stringify(g_tracks));
+		localStorage.setItem('createplaylist-name', g_name);
+		var w = window.open(url, 'asdf', 'WIDTH=400, HEIGHT=500');
+	}
+
+	var refreshtimer = 0;
+	var queueRefreshText = function() {
+		if (refreshtimer) {
+			clearTimeout(refreshtimer);
+		}
+		refreshtimer = setTimeout(function() {
+			refreshText();
+		}, 1000);
+	}
+
+	exports.startApp = function() {
+		setStatus('');
+		console.log('start app.');
+		$('#alltext').keyup(function() {
+			queueRefreshText();
+		});
+		$('#alltext').change(function() {
+			queueRefreshText();
+		});
+		$('#start').click(function() {
+			doLogin(function() {});
+		})
+		$('#alltext').text('hello world');
+		refreshText();
+		resolveOneWord();
+}
+
+})(window);
